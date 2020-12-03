fec6ba5770d1a6afe18697a304e90ca000ae7646
GaelFerrand <45355989+GaelFerrand@users.noreply.github.com>
2020-12-03 10:14:05 +0100

    Feat/ui top bar (#11)
    * feat: added top bar and recorder button
    
    * fix: fixed crashing be bug (url regex)
    
    * refactor: exported an inline function for perf and readability & updated joi error message to be consistent over OS's
    
    Co-authored-by: Gaël Ferrand <gael@getting-nutz.com>
    Co-authored-by: Axel <axeletcana@hotmail.fr>

---
+++ ammo-fe/src/components/Bar/index.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from 'react';
+import Box from '@material-ui/core/Box';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import RecorderButton from 'components/RecorderButton';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '74px',
+        width: '100%',
+        backgroundColor: theme.palette.secondary.dark,
+        position: 'fixed',
+        zIndex: 9999,
+        textAlign: 'center',
+        top: 0,
+    },
+}));
+
+const Bar = (): ReactElement => {
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.root} boxShadow={2} data-cy="bar">
+            <RecorderButton />
+        </Box>
+    );
+};
+
+export default Bar;
