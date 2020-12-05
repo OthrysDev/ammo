c0ad693b8fc5f9fcd95b9fff224436dc0666baf7
Axel <axeletcana@hotmail.fr>
2020-12-05 18:49:49 +0100

    feat: updated the ws hook to be a singleton and to expose some methods

--- ammo-fe/src/hooks/useWS.tsx
+++ ammo-fe/src/hooks/useWS.tsx
@@ -1,19 +1,34 @@
-import { useEffect } from 'react';
+import * as React from 'react';
+import { createContext, useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { Bullet } from 'shared/types/Bullet';
 import WS from 'network/WS';
 
-const useWS = (): void => {
-    const dispatch = useDispatch();
+type returnValue = {
+    init: () => void;
+    emit: (event: string, ...args: unknown[]) => void;
+    connected: boolean;
+};
 
+const WsContext = createContext({});
+
+const useProvideWS = (): returnValue => {
+    const dispatch = useDispatch();
+    const [connected, setConnected] = React.useState(false);
+
+    let initialized = false;
     const socket = WS.getSocket('http://localhost:3001');
 
-    useEffect(() => {
+    const init = (): void => {
+        if (initialized) return;
+        initialized = true;
+
         // This part is for the mocked WS, figure out a way to remove it
         if (socket.connected) dispatch({ type: 'CONNECTED' });
         else dispatch({ type: 'DISCONNECTED' });
 
         socket.on('connect', () => {
+            setConnected(true);
             dispatch({ type: 'CONNECTED' });
         });
 
@@ -24,7 +39,7 @@ const useWS = (): void => {
         // For a list of all reasons why the ws can disconnect : https://socket.io/docs/v3/client-api/#Event-%E2%80%98disconnect%E2%80%99
         socket.on('disconnect', (reason: string) => {
             dispatch({ type: 'DISCONNECTED' });
-
+            setConnected(false);
             if (reason === 'io server disconnect') {
                 // the disconnection was initiated by the server, you need to reconnect manually
                 socket.connect();
@@ -32,7 +47,28 @@ const useWS = (): void => {
             }
             // else the socket will automatically try to reconnect
         });
-    }, []);
+    };
+
+    const emit = (event: string, ...args: unknown[]): void =>
+        socket.emit(event, ...args);
+
+    return {
+        init,
+        emit,
+        connected,
+    };
 };
+
+export const ProvideWS = ({
+    children,
+}: {
+    children: JSX.Element;
+}): JSX.Element => {
+    const ws = useProvideWS();
+
+    return <WsContext.Provider value={ws}>{children}</WsContext.Provider>;
+};
+
+const useWS = (): returnValue => useContext(WsContext) as returnValue;
 
 export default useWS;
--- ammo-fe/src/index.tsx
+++ ammo-fe/src/index.tsx
@@ -8,17 +8,20 @@ import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import theme from 'material/Theme';
 import I18nProvider from 'redux/I18nProvider';
 import ErrorBoundary from 'components/ErrorBoundary';
+import { ProvideWS } from 'hooks/useWS';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <I18nProvider>
                 <ThemeProvider theme={theme}>
-                    <CssBaseline>
-                        <ErrorBoundary>
-                            <App />
-                        </ErrorBoundary>
-                    </CssBaseline>
+                    <ProvideWS>
+                        <CssBaseline>
+                            <ErrorBoundary>
+                                <App />
+                            </ErrorBoundary>
+                        </CssBaseline>
+                    </ProvideWS>
                 </ThemeProvider>
             </I18nProvider>
         </Provider>
