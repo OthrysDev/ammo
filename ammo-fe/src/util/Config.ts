2bf3f58c95be8a6776e45effb41aaa27e5efcc43
Gaël Ferrand <gael.ferrand@gmail.com>
2021-02-08 14:45:03 +0100

    refactor: removed 'return' from arrow function

--- ammo-fe/src/util/Config.ts
+++ ammo-fe/src/util/Config.ts
@@ -3,11 +3,9 @@ interface IConfig {
     port: number;
 }
 
-const buildConfig = (): IConfig => {
-    return {
-        beUrl: process.env.REACT_APP_BE_URL,
-        port: parseInt(process.env.PORT, 10),
-    };
-};
+const buildConfig = (): IConfig => ({
+    beUrl: process.env.REACT_APP_BE_URL,
+    port: parseInt(process.env.PORT, 10),
+});
 
 export default buildConfig();
